6ad325f85a4a20c8d5116890de39bde9a6d5690f
ahlanz <ahlangaffar@gmail.com>
2022-06-12 08:42:15 +0700

    hasil chalange-0

--- promise.js
+++ promise.js
@@ -1,7 +1,35 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    if (emosi === "marah") {
+      (async () => {
+        const sumIIX = count(await promiseTheaterIXX(), emosi);
+        const sumVGC = count (await promiseTheaterVGC(), emosi);
+        resolve(sumIIX + sumVGC);
+      })();
+    } else if (emosi === "tidak marah") {
+      const sumIX = count (await promiseTheaterIXX(), emosi); 
+      const sumVGC = count (await promiseTheaterVGC(), emosi);
+    } else {
+      reject("Emosi tidak valid");
+    }
+  });
+};
+
+const count = (data, emosi) => {
+  let count = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].hasil === emosi) {
+      count++;
+    };
+  };
+  return count;
+};
+
+
+
 
 module.exports = {
   promiseOutput,
